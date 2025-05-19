b3b4e9214b5d8ece5ea101278bf1b4d8e8020ea2
Naveen Rathi <102209027+Naveenrathi9@users.noreply.github.com>
2025-05-19 09:55:35 +0530

    Add files via upload

---
+++ it-request-system/backend/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config({ path: "./.env" });
+const connectDB = require("./config/db");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json());
+
+connectDB();
+
+const requestRoutes = require("./routes/requestRoutes");
+const authRoutes = require("./routes/authRoutes");
+app.use("/api", requestRoutes);
+app.use("/api", authRoutes);
+
+
+const path = require('path');
+const authMiddleware = require('./middleware/authMiddleware');
+
+app.get("/", (req, res) => {
+res.sendFile(path.join(__dirname, '../frontend/login.html'));
+  
+});
+
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
